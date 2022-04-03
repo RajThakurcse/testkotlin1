8880cbe9e29f819ffab0e7b229701d2051db60a2
RajThakurcse <thakur2022raj@gmail.com>
2022-04-03 16:11:30 +0530

    send1

--- src/pack1/packclass1exe.java
+++ src/pack1/packclass1exe.java
@@ -4,5 +4,6 @@ public class packclass1exe {
     public static void main(String[] args) {
         packclass1 o1=new packclass1();
         o1.fun1();
+        System.out.println("program ends");
     }
 }
